6905b6983c6ccacd76f8c5a4a3a249135991ce47
Paul Dennis <pauldennis.misc@gmail.com>
2017-01-28 16:42:48 -0500

    Disabled extra colors, added check for blank moves and insufficient palatte.

--- src/main/java/com/tiy/webapp/Color.java
+++ src/main/java/com/tiy/webapp/Color.java
@@ -4,5 +4,5 @@ package com.tiy.webapp;
  * Created by Paul Dennis on 1/19/2017.
  */
 public enum Color {
-    RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE
+    RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE
 }
